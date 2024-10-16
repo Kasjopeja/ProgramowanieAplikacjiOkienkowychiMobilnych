fad527d7f98a3bf03b6a013b9e3899e4f1eda6b0
Aleksandra <azakrecka@student.agh.edu.pl>
2024-10-16 10:48:57 +0200

    cosmetic changes

--- Lab1/src/Main.java
+++ Lab1/src/Main.java
@@ -3,6 +3,7 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
 
+        System.out.println("Chose option");
         Scanner scanner = new Scanner(System.in);
         int option = scanner.nextInt();
 
