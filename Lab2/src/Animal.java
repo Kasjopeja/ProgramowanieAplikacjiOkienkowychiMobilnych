24a16a43d78323f0f9ec4daa4dd808e4e726c2ce
Aleksandra <azakrecka@student.agh.edu.pl>
2024-10-23 12:54:31 +0200

    Animal and Enum

---
+++ Lab2/src/Animal.java
@@ -0,0 +1,8 @@
+public class Animal {
+    String name;
+    String species;
+    int age;
+    double price;
+    AnimalCondition condition;
+
+}
---
+++ Lab2/src/AnimalCondition.java
@@ -0,0 +1,3 @@
+public enum AnimalCondition{
+    Zdrowe, Chore, WTrakcieAdopcji, Kwarantanna
+}
